93fd75d101ac1d5defccc58bd3505a5813624a8f
Anton Ahatov <ahatov@gismeteo.ru>
2016-07-21 10:48:23 +0300

    web audio effect

--- 3-retrovideo/client/js/helpers/audioFilter.js
+++ 3-retrovideo/client/js/helpers/audioFilter.js
@@ -1,3 +1,13 @@
+export function lowpass(audio, level) {
+    const context = new AudioContext();
+    const filter = context.createBiquadFilter();
+    const source = context.createMediaElementSource(audio);
+
+    source.connect(filter);
+    filter.connect(context.destination);
+    filter.type = 'lowpass';
+    filter.frequency.value = level;
+}
 export function volume(source, level) {
     const noiseAudioContext = new AudioContext();
     const noiseBufferSize = 4096;
